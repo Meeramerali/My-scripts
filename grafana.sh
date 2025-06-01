bf2fbe00259ab5b2ea49d5beea32ac7c9411f441
root <root@ip-172-31-93-21.ec2.internal>
2025-06-01 16:27:18 +0000

    updated_grafana

--- grafana.sh
+++ grafana.sh
@@ -1,3 +1,6 @@
 apt-get install -y adduser libfontconfig1 musl
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise_12.0.1_amd64.deb
 dpkg -i grafana-enterprise_12.0.1_amd64.deb
+systemctl daemon-reload
+systemctl enable grafana-server
+service grafana-server start
